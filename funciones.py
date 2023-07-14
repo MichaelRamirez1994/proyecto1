f4fab0ccd06d86a138cdec1b260d657897435ea4
Michael Ferney Ramirez Campos <mfrc8470@coomeva.com.co>
2023-07-13 23:09:13 -0500

    13 julio 2023

---
+++ funciones.py
@@ -0,0 +1,21 @@
+#print ("hola estoy trabajando funciones en entornos git")
+#
+#def my_print():
+#    print ("esta es mi fucion")
+#    print ("esta es mi fucionde nuevo ")
+#my_print()
+
+# funcion nomrla 
+def imprimir_nombre():
+    mi_nombre= "papu"
+    print ("su nombre es: ", mi_nombre,id(mi_nombre), sep ="***")
+imprimir_nombre()
+
+# funcion enclosing 
+
+mi_nombre = "maiki"
+def imprimir_nombre():
+    global mi_nombre
+    mi_nombre= "papu"
+    print ("su nombre es: ", mi_nombre,id(mi_nombre), sep ="***")
+imprimir_nombre()
