40752063616e2158aaa3d31c43272f28369bea43
Michael Ferney Ramirez Campos <mfrc8470@coomeva.com.co>
2023-07-14 22:16:06 -0500

    14 julio 2023

---
+++ lambda.py
@@ -0,0 +1,17 @@
+def elevarcuadrado(num):
+    resultado = num * num
+    
+elevarcuadrado(5)
+
+elevarcuadrado = lambda num : num * num
+print ( "el resultado de 5 elevado al cuadrado es ", elevarcuadrado(5))
+
+############## funcion con return 
+
+def sumar(n1,n2):
+    return n1 + n2 
+print("la suma es : ", sumar(10,5))
+
+#######
+
+
---
+++ return.py
@@ -0,0 +1,17 @@
+# funciones normal invocada como siempre
+ 
+def multiplicacion (a,b):
+    return  (x)
+x = multiplicacion(2,5)
+
+
+print ("el resultado de la operacion es:", x)
+    
+
+
+
+
+
+
+
+
